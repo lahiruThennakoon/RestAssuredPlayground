1457aa97217122adc295487a3307a954b469cdd5
lahiru <lahiruthennakoon@gmail.com>
2025-03-07 19:52:58 +0530

    Enhance framework with RequestSpecification and specBuilder

--- src/test/java/org/testing/API/Basic.java
+++ src/test/java/org/testing/API/Basic.java
@@ -1,7 +1,13 @@
 package org.testing.API;
 
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 import org.testing.API.payload.CreatePlace;
 
 import static io.restassured.RestAssured.given;
@@ -12,11 +18,14 @@ public class Basic {
 
     public static void main(String[] args) {
 
+        RequestSpecification reqSpec = new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com").addQueryParam("key", "qaclick123")
+                .addHeader("content-Type", "application/json").build();
+        ResponseSpecification resSpec = new ResponseSpecBuilder().expectContentType(ContentType.JSON).expectStatusCode(200).
+                expectHeader("server", "Apache/2.4.52 (Ubuntu)").build();
 
-        RestAssured.baseURI = "https://rahulshettyacademy.com";
-        String response = given().queryParam("key", "qaclick123").headers("content-Type", "application/json").body(CreatePlace.payload())
-                .when().post("/maps/api/place/add/json")
-                .then().log().all().assertThat().statusCode(200).body("scope", equalTo("APP")).headers("server", "Apache/2.4.52 (Ubuntu)")
+        RequestSpecification requestSpecification = given().spec(reqSpec).body(CreatePlace.payload());
+        String response = requestSpecification.when().post("/maps/api/place/add/json")
+                .then().spec(resSpec).log().all()
                 .extract().response().asString();
 
         JsonPath js = new JsonPath(response);
@@ -24,17 +33,17 @@ public class Basic {
 
         String newAddress = "70 Ananda Mawatha, USA";
 
-        given().queryParam("key", "qaclick123").headers("content-Type", "application/json").body("{\n" +
+        given().spec(reqSpec).body("{\n" +
                         "\"place_id\":\"" + placeId + "\",\n" +
-                        "\"address\":\""+newAddress+"\",\n" +
+                        "\"address\":\"" + newAddress + "\",\n" +
                         "\"key\":\"qaclick123\"\n" +
-                        "}\n").when().put("/maps/api/place/update/json")
-                .then().log().all().assertThat().statusCode(200).body("msg", equalTo("Address successfully updated"));
+                        "}\n")
+                .when().put("/maps/api/place/update/json")
+                .then().spec(resSpec).log().all().body("msg", equalTo("Address successfully updated"));
 
-        given().queryParam("key", "qaclick123").queryParam("place_id", placeId).when().get("/maps/api/place/get/json")
-                .then().log().all().assertThat().statusCode(200).body("address", equalTo(newAddress));
+        given().spec(reqSpec).queryParam("place_id", placeId)
+                .when().get("/maps/api/place/get/json")
+                .then().log().all().spec(resSpec).body("address", equalTo(newAddress));
 
     }
-
-
 }
